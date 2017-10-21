8457afa0ec31ab74d3cc21d6201455b77d177830
luandro <luandro@gmail.com>
2017-10-21 18:39:56 -0200

    bugs on init

--- api/init.js
+++ api/init.js
@@ -1,28 +1,17 @@
-const defaults = {
-  info: {
-    id: 'net.maidsafe.test.webapp.id',
-    name: 'WebApp Test',
-    vendor: 'MaidSafe Ltd.'
-  },
-  permissions: {
-    _public: ['Insert'], // request to insert into `_public` container
-    _other: ['Insert', 'Update'] // request to insert and update in `_other` container
-  }
-}
-
 export default async function (appInfo, permissions, ownContainer) {
-  // Initialise
-  const appHandle = await window.safeApp.initialise({
-    id: appInfo.id || defaults.info.id,
-    name: appInfo.name || defaults.info.name,
-    vendor: appInfo.vendor || defaults.info.vendor
-  }, (newState) => {
-    console.log('Network state changed to: ', newState)
-  })
-  console.log('SAFEApp instance initialised and handle returned: ', appHandle)
-  // Authorise
-  const authUri = window.safeApp.authorise(appHandle, permissions || defaults.permissions, ownContainer || true)
-  // Get authorised session
-  await window.safeApp.connectAuthorised(appHandle, authUri)
-  console.log('The app was authorised & a session was created with the network')
+  try {
+    // Initialise
+    const appHandle = await window.safeApp.initialise({
+      id: appInfo.id,
+      name: appInfo.name,
+      vendor: appInfo.vendor
+    })
+    // Authorise
+    const authUri = await window.safeApp.authorise(appHandle, permissions, ownContainer)
+    // Get authorised session
+    await window.safeApp.connectAuthorised(appHandle, authUri)
+    console.log('The app was authorised & a session was created with the network')
+  } catch (err) {
+    console.log(err)
+  }
 }
